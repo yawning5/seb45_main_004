88dc4353bfe96a154d78d9e3fc87edf71c018add
Donga1125 <ehddk0133@naver.com>
2023-09-07 12:14:26 +0900

    충돌 수정

---
+++ server/src/test/java/com/party/ServerApplicationTests.java
@@ -0,0 +1,15 @@
+package com.party;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
