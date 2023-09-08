6bfb0892e14c7388a531ce0222beafb29e7f579c
yawning5 <yawning055@gmail.com>
2023-09-08 12:10:19 +0900

    Update ServerApplication.java

--- server/src/main/java/com/party/ServerApplication.java
+++ server/src/main/java/com/party/ServerApplication.java
@@ -3,6 +3,8 @@ package com.party;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// 테스트
+
 @SpringBootApplication
 public class ServerApplication {
 
